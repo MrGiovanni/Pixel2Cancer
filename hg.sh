ced6c4409986e370807bc4a0e3582df6bca177ed
Yuxiang Lai <88482687+Arbiter00@users.noreply.github.com>
2023-09-15 14:06:14 -0400

    Update hg.sh

--- hg.sh
+++ hg.sh
@@ -19,7 +19,7 @@ module load mamba/latest
 source activate pixel2cancer
 
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
-pip install -r requirement.txt
+pip install -r requirements.txt
 
 cd ./Tumor_Synthesis/cellular
 python setup.py install
