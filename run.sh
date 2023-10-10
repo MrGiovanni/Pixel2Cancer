d9a74384df8f01f1fd61c26f41eaec87e684af06
Zongwei Zhou <zzhou82@jh.edu>
2023-10-09 20:56:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,3 +6,9 @@ sbatch --error=logs/$1.out --output=logs/$1.out hg.sh $1
 
 # ## 2. Train segmentation models using real tumors (for comparison)
 # for config in real.no_pretrain.unet real.pretrain.swin_unetrv2_base real.no_pretrain.swin_unetrv2_base real.no_pretrain.swin_unetrv2_small real.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config; done
+
+# ## 3. Evaluate synt-tumor models using real tumors
+# for config in eval.synt.no_pretrain.unet eval.synt.pretrain.swin_unetrv2_base eval.synt.no_pretrain.swin_unetrv2_base eval.synt.no_pretrain.swin_unetrv2_small eval.synt.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config; done
+
+# ## 4. Evaluate real-tumor models using real tumors
+# for config in eval.real.no_pretrain.unet eval.real.pretrain.swin_unetrv2_base eval.real.no_pretrain.swin_unetrv2_base eval.real.no_pretrain.swin_unetrv2_small eval.real.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config; done
