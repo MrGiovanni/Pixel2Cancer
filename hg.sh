dbaaa8b2750533039e8370927da77c3546d58282
Zongwei Zhou <zzhou82@jh.edu>
2023-10-12 10:27:11 -0400

    Update hg.sh

--- hg.sh
+++ hg.sh
@@ -20,6 +20,9 @@ source activate pixel2cancer
 
 # pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
 # pip install -r requirements.txt
+# cd ./surface-distance
+# pip install .
+# cd ..
 
 # cd ./Tumor_Synthesis/cellular
 # python setup.py install
