5ce67a8b2725bb00d69c087644c755f9a3120e7c
Yuxiang Lai <88482687+Arbiter00@users.noreply.github.com>
2023-10-20 01:50:46 +0800

    Update cellular.py
    death

--- Tumor_Synthesis/cellular/cellular.py
+++ Tumor_Synthesis/cellular/cellular.py
@@ -2,11 +2,11 @@ import torch
 from Cellular import _C
 
 
-def update_cellular(state_tensor, density_state_tensor, ranges, thresholds, grow_per_cell=1, max_try=-1):
+def update_cellular(state_tensor, density_state_tensor, ranges, thresholds, flag, grow_per_cell=1, max_try=-1):
     if max_try < 0:
         max_try = grow_per_cell * 3
     
-    return _CellularUpdate.apply(ranges, grow_per_cell, max_try, state_tensor, density_state_tensor, thresholds)
+    return _CellularUpdate.apply(ranges, grow_per_cell, max_try, state_tensor, density_state_tensor, thresholds, flag)
 
 
 class _CellularUpdate(torch.autograd.Function):
@@ -17,12 +17,13 @@ class _CellularUpdate(torch.autograd.Function):
                 max_try,
                 state_tensor,
                 density_state_tensor,
-                thresholds
+                thresholds,
+                flag
         ):
         Y_range, X_range, Z_range = ranges
         organ_hu_lowerbound, organ_standard_val, outrange_standard_val, threshold = thresholds
         state_tensor_new = state_tensor.clone()
-        _C.update_cellular(state_tensor, density_state_tensor, Y_range, X_range, Z_range, grow_per_cell, max_try, organ_hu_lowerbound, organ_standard_val, outrange_standard_val, threshold, state_tensor_new)
+        _C.update_cellular(state_tensor, density_state_tensor, Y_range, X_range, Z_range, grow_per_cell, max_try, organ_hu_lowerbound, organ_standard_val, outrange_standard_val, threshold, flag, state_tensor_new)
         return state_tensor_new
 
 if __name__ == '__main__':
