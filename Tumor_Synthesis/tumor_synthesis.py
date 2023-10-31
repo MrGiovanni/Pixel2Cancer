c9b06bdb67244afd4636d5f2734b29275786aab2
Yuxiang Lai <88482687+Arbiter00@users.noreply.github.com>
2023-10-31 23:10:00 +0800

    Update tumor_synthesis.py pancreas

--- Tumor_Synthesis/tumor_synthesis.py
+++ Tumor_Synthesis/tumor_synthesis.py
@@ -7,9 +7,10 @@ from monai.transforms.transform import MapTransform, RandomizableTransform
 from .utils import generate_tumor,get_predefined_texture
 import numpy as np
 
-Organ_List = {'liver': [1,2]}
-Organ_HU = {'liver': [100, 160]}
 
+Organ_List = {'liver': [1,2], 'pancreas': [1,2]}
+Organ_HU = {'liver': [100, 160],'pancreas': [100, 160]}
+steps = {'liver': 150, 'pancreas': 100}
 class TumorSysthesis(RandomizableTransform, MapTransform):
     def __init__(self, 
     keys: KeysCollection, 
@@ -21,19 +22,20 @@ class TumorSysthesis(RandomizableTransform, MapTransform):
         RandomizableTransform.__init__(self, prob)
         random.seed(0)
         np.random.seed(0)
-        
-        self.steps = 150  # step
+        self.args = args
+        self.organ_name = args.organ  # organ name
+        self.steps = steps[args.organ]  # step
         self.kernel_size = (3, 3, 3)  # Receptive Field
-        self.organ_hu_lowerbound = Organ_HU['liver'][0]  # organ hu lowerbound
-        self.outrange_standard_val = Organ_HU['liver'][1]  # outrange standard value
+        self.organ_hu_lowerbound = Organ_HU[args.organ][0]  # organ hu lowerbound
+        self.outrange_standard_val = Organ_HU[args.organ][1]  # outrange standard value
         self.organ_standard_val = 0  # organ standard value
         self.threshold = 10  # threshold
-        self.organ_name = 'liver'  # organ name
-        self.args = args
+        
+        
         self.textures = []
         sigma_as = [3, 6, 9, 12, 15]
         sigma_bs = [4, 7]
-        predefined_texture_shape = (420, 300, 320)
+        predefined_texture_shape = (500, 500, 500)
         for sigma_a in sigma_as:
             for sigma_b in sigma_bs:
                 texture = get_predefined_texture(predefined_texture_shape, sigma_a, sigma_b)
@@ -46,6 +48,11 @@ class TumorSysthesis(RandomizableTransform, MapTransform):
         d = dict(data)
         self.randomize(None)
 
+        if self._do_transform and (np.max(d['label']) <= 1):
+            texture = random.choice(self.textures)
+            d['image'][0], d['label'][0] = generate_tumor(d['image'][0], d['label'][0],texture, self.steps, self.kernel_size, self.organ_standard_val, self.organ_hu_lowerbound, self.outrange_standard_val, self.threshold, self.organ_name, self.args)
+        
+        return d
         if self._do_transform and (np.max(d['label']) <= 1):
             texture = random.choice(self.textures)
             d['image'][0], d['label'][0] = generate_tumor(d['image'][0], d['label'][0],texture, self.steps, self.kernel_size, self.organ_standard_val, self.organ_hu_lowerbound, self.outrange_standard_val, self.threshold, self.organ_name, self.args)
