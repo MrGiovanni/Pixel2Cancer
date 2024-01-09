2cf665cb5619e953c278dc5f7478187a387e9cf7
Yuxiang Lai <88482687+Arbiter00@users.noreply.github.com>
2024-01-10 01:30:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,8 @@ sbatch --error=logs/$1.$2.out --output=logs/$1.$2.out "hg_$2_train_5_folds.sh" $
 # ## 1. Train segmentation models using synthetic tumors
 # for organ in kidney liver pancreas; do for fold in 1 2 3 4 5; do for config in synt.$organ.fold$fold.no_pretrain.unet synt.$organ.fold$fold.pretrain.swin_unetrv2_base synt.$organ.fold$fold.no_pretrain.swin_unetrv2_base synt.$organ.fold$fold.no_pretrain.swin_unetrv2_small synt.$organ.fold$fold.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config $organ; done; done; done
 
-# for organ in kidney; do for fold in 1 2; do for config in synt.$organ.fold$fold.no_pretrain.unet synt.$organ.fold$fold.pretrain.swin_unetrv2_base synt.$organ.fold$fold.no_pretrain.swin_unetrv2_base synt.$organ.fold$fold.no_pretrain.swin_unetrv2_small synt.$organ.fold$fold.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config $organ; done; done; done
-
 # ## 2. Train segmentation models using real tumors (for comparison)
-# for config in real.no_pretrain.unet real.pretrain.swin_unetrv2_base real.no_pretrain.swin_unetrv2_base real.no_pretrain.swin_unetrv2_small real.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config; done
+# for organ in kidney liver pancreas; do for fold in 1 2 3 4 5; do for config in real.$organ.fold$fold.no_pretrain.unet real.$organ.fold$fold.pretrain.swin_unetrv2_base real.$organ.fold$fold.no_pretrain.swin_unetrv2_base real.$organ.fold$fold.no_pretrain.swin_unetrv2_small real.$organ.fold$fold.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config $organ; done; done; done
 
 # ## 3. Evaluate synt-tumor models using real tumors
 # for config in eval.synt.no_pretrain.unet eval.synt.pretrain.swin_unetrv2_base eval.synt.no_pretrain.swin_unetrv2_base eval.synt.no_pretrain.swin_unetrv2_small eval.synt.no_pretrain.swin_unetrv2_tiny; do bash run.sh $config; done
