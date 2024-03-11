cdf2e14ce0f66fdd9355516ebae74bf024471dcd
Yuxiang Lai <88482687+Arbiter00@users.noreply.github.com>
2024-03-11 13:33:13 +0800

    Update cellular.cu

--- Tumor_Synthesis/cellular/csrc/cellular.cu
+++ Tumor_Synthesis/cellular/csrc/cellular.cu
@@ -23,19 +23,19 @@ __device__ int check_valid(
     return 1;
 }
 
-// interaction with organ tissue
+// interaction with normal organ tissue
 __device__ int check_tissue(
     const int target_val,
     const int organ_hu_lowerbound, 
     const int interval,
     const int density_probability
 ){
-    // seprate tissue area into 3 level
+    // 3 level of tissue area
     const int interval_1 = organ_hu_lowerbound;
     const int interval_2 = organ_hu_lowerbound + interval;
     const int interval_3 = organ_hu_lowerbound + interval * 2;
 
-    // probability of grow in tissue area
+    // probability of grow in each level organ tissue
     if(target_val == interval_1){
         if (density_probability <= 1) return 1;
     }
@@ -57,10 +57,10 @@ __device__ int mass_effect(
     const int organ_standard_val,
     const int threshold
 ){
-    // check the density of current tissue
+    // check whether the population of current cell is higher than the boundary/vessels invasion threshold
     if (curr_val < threshold * 10/10){
         return 1;
-    }// check the density of target tissue
+    }
     else {
         return 0;
     }
@@ -89,7 +89,7 @@ __global__ void UpdateCellularKernel(
     const int tid = blockIdx.x * blockDim.x + threadIdx.x;
     const int interval = (outrange_standard_val - organ_hu_lowerbound) / 3;
     
-    
+    // Growth and Invasion
     if (flag == false){
         for (int pid = tid; pid < H * W * D; pid += num_threads) {
             const int curr_val = state_tensor_prev[pid];
@@ -103,9 +103,9 @@ __global__ void UpdateCellularKernel(
             const int x = (pid % (W * D)) / D;
             const int z = pid % D;
 
-            // proliferative cell 
+            // proliferative cell
             if (curr_val < threshold * 5/10){
-
+                // self-state + 1
                 atomicAdd(state_tensor + (y) * (W * D) + (x) * D + (z), 1);
                 continue;
             }
@@ -122,6 +122,7 @@ __global__ void UpdateCellularKernel(
             int n_try = 0;
 
             while (finished < grow_per_cell && n_try < max_try){
+                // random select a target neighbor
                 // state += n_try * 7;
                 curand_init(clock64(), tid, 0, &state);
                 current_select = curand_uniform(&state) * (Y_range * X_range * Z_range + 0.9999);
@@ -141,19 +142,22 @@ __global__ void UpdateCellularKernel(
                 if (check_valid(H, W, D, y + y_shift, x + x_shift, z + z_shift) == 0){
                     continue;
                 }
+
                 // if you want to add more rule to avoid cellular grown in certain area:
                 // if (...condition...) {
                 // continue;   
                 // }
 
             
-                // mass effect
+                // Simulate interaction with vessel and organ boundary
                 if (mass_effect(curr_val, state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)], outrange_standard_val, organ_standard_val, threshold) == 0){
                     // Get target value
+                    // if the pressure is higher than the threshold, then can invade and squeeze the vessel and organ boundary
                     if (state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)] > (outrange_standard_val + 2)){
                         state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)] = organ_standard_val;
                         density_state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)] = organ_hu_lowerbound;
                     }
+                    // Apply pressure to the vessel and organ boundary
                     else if (state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)] >= (outrange_standard_val)){
                         atomicAdd(state_tensor + (y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift), 1);
                         // finished ++;
@@ -162,9 +166,9 @@ __global__ void UpdateCellularKernel(
                 }
 
 
-                // detect tissue area
-                // interaction with tissue
+                // Simulate interaction with normal organ tissue
                 curand_init(clock64(), tid, 0, &state);
+                // invasion probability
                 density_probability = curand_uniform(&state);
                 if (check_tissue(density_state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)], organ_hu_lowerbound, interval, density_probability) == 0){
                     continue;
@@ -179,19 +183,20 @@ __global__ void UpdateCellularKernel(
 
             
 
-                // connot grow in the max value area
+                // connot grow in the max population area
                 if (state_tensor[(y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift)] >= threshold){
                     continue;
                 }
 
 
-                // grow
+                // grow to neiguhor
                 atomicAdd(state_tensor + (y + y_shift) * (W * D) + (x + x_shift) * D + (z + z_shift), 1);
                 finished ++;
             }
         }
 
     }
+    // Death
     else if(flag == true){
         for (int pid = tid; pid < H * W * D; pid += num_threads) {
             const int curr_val = state_tensor_prev[pid];
@@ -244,7 +249,8 @@ __global__ void UpdateCellularKernel(
     
 }
 
-
+// C++ interface for the CUDA kernel
+// initialize the state_tensor
 at::Tensor UpdateCellular(
     const at::Tensor& state_tensor_prev,
     const at::Tensor& density_state_tensor,
@@ -267,13 +273,14 @@ at::Tensor UpdateCellular(
     const int H = state_tensor.size(0);
     const int W = state_tensor.size(1);
     const int D = state_tensor.size(2);
-
+    
     const size_t blocks = 1024;
     const size_t threads = 64;
     
     // const size_t blocks = 32;
     // const size_t threads = 8;
 
+    // Launch the cuda kernel
     UpdateCellularKernel<<<blocks, threads, 0, stream>>>(
         state_tensor_prev.contiguous().data_ptr<int>(),
         density_state_tensor.contiguous().data_ptr<int>(),
